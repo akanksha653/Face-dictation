10904469bbaf8038eab72b12397f2ab654e88b19
Akanksha Mishra <akankmis644@gmail.com>
2025-05-29 07:08:38 +0530

    Create __init__.py

---
+++ face_recognition/__init__.py
@@ -0,0 +1,8 @@
+
+# -*- coding: utf-8 -*-
+
+__author__ = """Akanksha Mishra"""
+__email__ = 'nehamis2511@gmail.com'
+__version__ = '1.4.0'
+
+from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
